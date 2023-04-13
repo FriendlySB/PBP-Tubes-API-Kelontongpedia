bea2723d7c3407eea440753bccca0ab3c6db7891
grald003 <josephgeraldo2003@gmail.com>
2023-04-13 23:07:35 +0700

    Fix ReviewItem

--- controller/review_controller.go
+++ controller/review_controller.go
@@ -74,7 +74,7 @@ func ReviewItem(w http.ResponseWriter, r *http.Request) {
 		var check bool
 		query := "SELECT COUNT(*) "
 		query += "FROM users u "
-		query += "INNER JOIN transaction t ON t.userId=u.userid"
+		query += "INNER JOIN transaction t ON t.userId=u.userid "
 		query += "INNER JOIN transaction_detail td ON td.transactionId=t.transactionId "
 		query += "INNER JOIN item i ON i.itemId=td.itemId "
 		query += "WHERE u.userid=? AND i.itemId=? AND t.progress='Done'"
