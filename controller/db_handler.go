898168ba0e1cb74e49b5de3f371ff7d7cf9fb49f
FriendlySB <bunardif@gmail.com>
2023-04-27 11:39:57 +0700

    Tambah env

--- controller/db_handler.go
+++ controller/db_handler.go
@@ -2,11 +2,19 @@ package controller
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/tubes_pbp?charset=utf8mb4&parseTime=True&loc=Local")
+	_ = godotenv.Load()
+	db_port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_NAME")
+	dbaddress := fmt.Sprintf("root:@tcp(127.0.0.1:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_port, db_name)
+	db, err := sql.Open("mysql", dbaddress)
 	if err != nil {
 		log.Fatal()
 	}
